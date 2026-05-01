87f72aeb83aa70d69829ede243d9152f7f411d8a
makkis10 <makkis.modding@gmail.com>
2026-05-01 15:09:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,40 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
+MY_LAT = 26.735729#60.169857
+MY_LONG = 85.928291#24.938379
+timestamp_count = 4
+api_key = os.environ.get("OMW_API_KEY")
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+parameters = {
+        "lat": MY_LAT,
+        "lon": MY_LONG,
+        "cnt": timestamp_count,
+        "appid": api_key,
+    }
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for timestamp in weather_data["list"]:
+    if timestamp["weather"][0]["id"] < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="It's going to rain today. Remember to bring an umbrella. ☂️☂️☂️☂️",
+        to="whatsapp:358406788102"
+    )
+    print(message.status)
